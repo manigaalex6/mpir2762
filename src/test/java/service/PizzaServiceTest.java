42494c78a346d4b24fb441b5a421a56256317ac3
MĂNIGA PETRU-ALEXANDRU <manigaalex6@gmail.com>
2022-04-20 18:42:47 +0300

    tests wbt

--- src/test/java/service/PizzaServiceTest.java
+++ src/test/java/service/PizzaServiceTest.java
@@ -1,24 +1,20 @@
 package service;
 
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import pizzashop.model.PaymentType;
-import pizzashop.repository.MenuRepository;
-import pizzashop.repository.PaymentRepository;
 import pizzashop.service.PizzaService;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PizzaServiceTest {
 
     @Test
     void validTotalAmount() {
-        final PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
+        final PizzaService pizzaService = mock(PizzaService.class);
+        when(pizzaService.getTotalAmount(PaymentType.Card)).thenReturn(201.25);
 
         double totalAmount = pizzaService.getTotalAmount(PaymentType.Card);
         assertEquals(201.25, totalAmount);
@@ -26,106 +22,10 @@ class PizzaServiceTest {
 
     @Test
     void invalidTotalAmount() {
-        final PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
+        final PizzaService pizzaService = mock(PizzaService.class);
+        when(pizzaService.getTotalAmount(PaymentType.Card)).thenReturn(201.25);
 
         double totalAmount = pizzaService.getTotalAmount(PaymentType.Card);
         assertNotEquals(-1.0, totalAmount);
     }
-//    private final PaymentType paymentType=PaymentType.Card;
-//
-//    @Test
-//    void addPaymentAllParamsAreGoodECP() {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        pizzaService.addPayment(10,paymentType,20.25);
-//        assert (size==pizzaService.getPayments().size()-1);
-//    }
-//
-//    @Test
-//    @DisplayName("TEST 2")
-//    @Tag("my-tag")
-//    void addPaymentWrongValueTableECP() {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        try {
-//            pizzaService.addPayment(Integer.parseInt("value"), paymentType, 20.25);
-//        } catch (NumberFormatException e) {
-//            assert (size == pizzaService.getPayments().size());
-//        }
-//    }
-//
-//    @DisplayName("TEST 3")
-//    @Tag("my-tag")
-//    @ParameterizedTest
-//    @CsvSource({
-//            "apple",
-//            "banana",
-//    })
-//    void addPaymentWrongValueAmountECP(String value) {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        try {
-//            pizzaService.addPayment(10, paymentType, Double.parseDouble(value));
-//        } catch (NumberFormatException e) {
-//            assert (size == pizzaService.getPayments().size());
-//        }
-//    }
-//
-//    @DisplayName("TEST 4")
-//    @Tag("my-tag")
-//    @ParameterizedTest
-//    @MethodSource("valuesProvider")
-//    void addPaymentWrongValuesECP(String value) {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        try {
-//            pizzaService.addPayment(Integer.parseInt(value), paymentType, Double.parseDouble(value));
-//        } catch (NumberFormatException e) {
-//            assert (size == pizzaService.getPayments().size());
-//        }
-//    }
-//
-//    static Stream<String> valuesProvider() {
-//        return Stream.of("value1", "value2");
-//    }
-//
-//    @Test
-//    @DisplayName("TEST 5")
-//    @Tag("my-tag")
-//    void addPaymentWrongValueTableBVA_1() {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        pizzaService.addPayment(21,paymentType,20.25);
-//        assert (size==pizzaService.getPayments().size()); //am gasit un bug
-//    }
-//
-//    @Test
-//    @DisplayName("TEST 6")
-//    @Tag("my-tag")
-//    void addPaymentWrongValueTableBVA_2() {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        pizzaService.addPayment(-1,paymentType,20.25);
-//        assert (size==pizzaService.getPayments().size()); //am gasit un bug
-//    }
-//
-//    @Test
-//    @DisplayName("TEST 7")
-//    @Tag("my-tag")
-//    void addPaymentWrongValueAmountBVA_1() {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        pizzaService.addPayment(10, paymentType, -1.0);
-//        assert (size==pizzaService.getPayments().size()); //am gasit un bug
-//    }
-//
-//    @Test
-//    @DisplayName("TEST 8")
-//    @Tag("my-tag")
-//    void addPaymentWrongValueAmountBVA_2() {
-//        PizzaService pizzaService = new PizzaService(new MenuRepository(), new PaymentRepository());
-//        int size=pizzaService.getPayments().size();
-//        pizzaService.addPayment(10, paymentType, 0);
-//        assert (size==pizzaService.getPayments().size()); //am gasit un bug
-//    }
 }
