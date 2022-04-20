da08ceac951524f4f71c3a1cbdeb2ac27bf48c1c
MĂNIGA PETRU-ALEXANDRU <manigaalex6@gmail.com>
2022-04-20 18:53:49 +0300

    tests bbt

--- src/test/java/service/PizzaServiceTest.java
+++ src/test/java/service/PizzaServiceTest.java
@@ -6,8 +6,7 @@ import pizzashop.service.PizzaService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class PizzaServiceTest {
 
@@ -27,5 +26,137 @@ class PizzaServiceTest {
 
         double totalAmount = pizzaService.getTotalAmount(PaymentType.Card);
         assertNotEquals(-1.0, totalAmount);
+    }
+
+    @Test
+    void invalidTableNumberTestCase() {
+        final PizzaService pizzaService = mock(PizzaService.class);
+
+        final int table = -1;
+        final PaymentType paymentType = PaymentType.Card;
+        final double amount = 100.;
+
+        doAnswer(invocationOnMock -> {
+            assertNotEquals(1, table);
+            assertEquals(paymentType, PaymentType.Card);
+            assertEquals(100., amount);
+
+            return null;
+        }).when(pizzaService).addPayment(table, paymentType, amount);
+
+        pizzaService.addPayment(table, paymentType, amount);
+
+        verify(pizzaService, times(1))
+                .addPayment(table, paymentType, amount);
+    }
+
+    @Test
+    void validTableNumberTestCase() {
+        final PizzaService pizzaService = mock(PizzaService.class);
+
+        final int table = 1;
+        final PaymentType paymentType = PaymentType.Card;
+        final double amount = 100.;
+
+        doAnswer(invocationOnMock -> {
+            assertEquals(1, table);
+            assertEquals(paymentType, PaymentType.Card);
+            assertEquals(100., amount);
+
+            return null;
+        }).when(pizzaService).addPayment(table, paymentType, amount);
+
+        pizzaService.addPayment(table, paymentType, amount);
+
+        verify(pizzaService, times(1))
+                .addPayment(table, paymentType, amount);
+    }
+
+    @Test
+    void validPaymentTestCase() {
+        final PizzaService pizzaService = mock(PizzaService.class);
+
+        final int table = 1;
+        final PaymentType paymentType = PaymentType.Card;
+        final double amount = 100.;
+
+        doAnswer(invocationOnMock -> {
+            assertEquals(1, table);
+            assertEquals(paymentType, PaymentType.Card);
+            assertEquals(100., amount);
+
+            return null;
+        }).when(pizzaService).addPayment(table, paymentType, amount);
+
+        pizzaService.addPayment(table, paymentType, amount);
+
+        verify(pizzaService, times(1))
+                .addPayment(table, paymentType, amount);
+    }
+
+    @Test
+    void invalidPaymentTestCase() {
+        final PizzaService pizzaService = mock(PizzaService.class);
+
+        final int table = 1;
+        final PaymentType paymentType = PaymentType.Cash;
+        final double amount = 100.;
+
+        doAnswer(invocationOnMock -> {
+            assertEquals(1, table);
+            assertNotEquals(paymentType, PaymentType.Card);
+            assertEquals(100., amount);
+
+            return null;
+        }).when(pizzaService).addPayment(table, paymentType, amount);
+
+        pizzaService.addPayment(table, paymentType, amount);
+
+        verify(pizzaService, times(1))
+                .addPayment(table, paymentType, amount);
+    }
+
+    @Test
+    void validAmountTestCase () {
+        final PizzaService pizzaService = mock(PizzaService.class);
+
+        final int table = 1;
+        final PaymentType paymentType = PaymentType.Cash;
+        final double amount = 100.;
+
+        doAnswer(invocationOnMock -> {
+            assertEquals(1, table);
+            assertNotEquals(paymentType, PaymentType.Card);
+            assertEquals(100., amount);
+
+            return null;
+        }).when(pizzaService).addPayment(table, paymentType, amount);
+
+        pizzaService.addPayment(table, paymentType, amount);
+
+        verify(pizzaService, times(1))
+                .addPayment(table, paymentType, amount);
+    }
+
+    @Test
+    void invalidAmountTestCase () {
+        final PizzaService pizzaService = mock(PizzaService.class);
+
+        final int table = 1;
+        final PaymentType paymentType = PaymentType.Cash;
+        final double amount = -100.;
+
+        doAnswer(invocationOnMock -> {
+            assertEquals(1, table);
+            assertNotEquals(paymentType, PaymentType.Card);
+            assertNotEquals(100., amount);
+
+            return null;
+        }).when(pizzaService).addPayment(table, paymentType, amount);
+
+        pizzaService.addPayment(table, paymentType, amount);
+
+        verify(pizzaService, times(1))
+                .addPayment(table, paymentType, amount);
     }
 }
